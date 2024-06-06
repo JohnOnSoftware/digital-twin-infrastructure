4c1a84e14256a5a6877d93af67dccfd9faa2f208
Zhong Wu <wuzhong1980@gmail.com>
2024-06-06 08:43:23 -0400

    migrate to new SDK

--- routes/common/oauth.js
+++ routes/common/oauth.js
@@ -16,51 +16,28 @@
 // UNINTERRUPTED OR ERROR FREE.
 /////////////////////////////////////////////////////////////////////
 
-const { AuthClientTwoLegged } = require('forge-apis');
+const { SdkManagerBuilder } = require('@aps_sdk/autodesk-sdkmanager');
+const { AuthenticationClient, Scopes } = require('@aps_sdk/authentication');
 
 const config = require('../../config');
 
-/**
- * Initializes a Forge client for 2-legged authentication.
- * @param {string[]} scopes List of resource access scopes.
- * @returns {AuthClientTwoLegged} 2-legged authentication client.
- */
-function getClient(scopes) {
-    const { client_id, client_secret } = config.credentials;
-    return new AuthClientTwoLegged(client_id, client_secret, scopes || config.scopes.internal);
-}
 
-let cache = {};
-async function getToken(scopes) {
-    const key = scopes.join('+');
-    if (cache[key]) {
-        return cache[key];
-    }
-    const client = getClient(scopes);
-    let credentials = await client.authenticate();
-    cache[key] = credentials;
-    setTimeout(() => { delete cache[key]; }, credentials.expires_in * 1000);
-    return credentials;
-}
+const sdk = SdkManagerBuilder.create().build();
+const authenticationClient = new AuthenticationClient(sdk);
+
 
 /**
  * Retrieves a 2-legged authentication token for preconfigured public scopes.
  * @returns Token object: { "access_token": "...", "expires_at": "...", "expires_in": "...", "token_type": "..." }.
  */
 async function getPublicToken() {
-    return getToken(config.scopes.public);
+    const credentials = await authenticationClient.getTwoLeggedToken(config.credentials.client_id, config.credentials.client_secret, [
+        Scopes.DataRead
+    ]);
+    return credentials;
 }
 
-/**
- * Retrieves a 2-legged authentication token for preconfigured internal scopes.
- * @returns Token object: { "access_token": "...", "expires_at": "...", "expires_in": "...", "token_type": "..." }.
- */
-async function getInternalToken() {
-    return getToken(config.scopes.internal);
-}
 
 module.exports = {
-    getClient,
     getPublicToken,
-    getInternalToken
 };
--- start.js
+++ start.js
@@ -30,8 +30,6 @@ let app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json({ limit: '50mb' }));
 app.use('/api/forge/oauth', require('./routes/oauth'));
-app.use('/api/forge/oss', require('./routes/oss'));
-app.use('/api/forge/modelderivative', require('./routes/modelderivative'));
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(err.statusCode).json(err);
